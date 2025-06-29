9dd28aa484515967c8f02f6bb99ce6823036e0ac
jahnavikannuru <jahnavikannuru@gmail.com>
2025-06-29 23:45:50 +0530

    EXERCISE 4

---
+++ WEEK 2/JUNIT/EXERCISE4.java
@@ -0,0 +1,10 @@
+package com.deepskilling;
+
+public class EXERCISE4 {
+	public double computeSquareArea(double length) {
+		return length*length;
+	}
+	public double computeCircleArea(double radius) {
+		return 3.14*radius*radius;
+	}
+}
