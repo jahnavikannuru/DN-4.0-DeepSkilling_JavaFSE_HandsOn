baa5cd12a9528ce0beac227810f319612189b744
jahnavikannuru <jahnavikannuru@gmail.com>
2025-07-09 19:07:38 +0530

    Add files via upload

---
+++ WEEK 3/SPRING CORE/EXERCISE 8/BookRepository.java
@@ -0,0 +1,7 @@
+package com.library.repository;
+
+public class BookRepository {
+    public void bookRepo(){
+        System.out.println("This is book repository");
+    }
+}
---
+++ WEEK 3/SPRING CORE/EXERCISE 8/BookService.java
@@ -0,0 +1,16 @@
+package com.library.service;
+import com.library.repository.BookRepository;
+public class BookService {
+    public BookRepository bookRepository;
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+    public void book() {
+        System.out.println("This is a book service");
+        bookRepository.bookRepo();
+    }
+}
+
