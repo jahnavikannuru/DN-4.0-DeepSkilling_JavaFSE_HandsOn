1323fffcd6c486b1fe88d6403bcc648173bad8d0
jahnavikannuru <jahnavikannuru@gmail.com>
2025-07-10 13:06:09 +0530

    Add files via upload

---
+++ WEEK 3/SPRING JPA AND HIBERNATE/1/HANDSON 7/CountryService.java
@@ -0,0 +1,27 @@
+package com.cognizant.orm_learn.service;
+
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import com.cognizant.orm_learn.model.Country;
+import com.cognizant.orm_learn.repository.CountryRepository;
+import com.cognizant.orm_learn.service.exception.CountryNotFoundException;
+
+@Service
+public class CountryService {
+    @Autowired
+    private CountryRepository countryRepository;
+    @Transactional
+    public Country findCountryByCode(String countryCode) throws CountryNotFoundException {
+        Optional<Country> result = countryRepository.findById(countryCode);
+        if (!result.isPresent()) {
+            throw new CountryNotFoundException("Country not found with code: " + countryCode);
+        }
+        return result.get();
+    }
+    @Transactional
+    public void addCountry(Country country) {
+        countryRepository.save(country);
+    } 
+}
---
+++ WEEK 3/SPRING JPA AND HIBERNATE/1/HANDSON 7/OrmLearnApplication.java
@@ -0,0 +1,41 @@
+package com.cognizant.orm_learn;
+
+import org.springframework.context.ApplicationContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.cognizant.orm_learn.model.Country;
+import com.cognizant.orm_learn.service.CountryService;
+import com.cognizant.orm_learn.service.exception.CountryNotFoundException;
+
+@SpringBootApplication
+public class OrmLearnApplication {
+    private static CountryService countryService;
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrmLearnApplication.class);
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(OrmLearnApplication.class, args);
+        countryService = context.getBean(CountryService.class);
+        try {
+            getCountryByCodeTest();
+        } catch (CountryNotFoundException e) {
+            LOGGER.error("Exception: {}", e.getMessage());
+        }
+        testAddCountry();
+    }
+    private static void getCountryByCodeTest() throws CountryNotFoundException {
+        LOGGER.info("Start");
+        Country country = countryService.findCountryByCode("IN");
+        LOGGER.info("Country: {}", country);
+        LOGGER.info("End");
+    }
+    private static void testAddCountry(){
+        LOGGER.info("Start");
+        Country newCountry = new Country();
+        newCountry.setCode("II");
+        newCountry.setName("inland");
+        countryService.addCountry(newCountry);
+        LOGGER.info("Country added.");
+        LOGGER.info("End");
+    }
+}
