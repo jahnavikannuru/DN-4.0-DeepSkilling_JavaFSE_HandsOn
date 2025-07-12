c46527be3a462d654c92dfc25a8be143202cd551
jahnavikannuru <jahnavikannuru@gmail.com>
2025-07-12 22:42:38 +0530

    Add files via upload

---
+++ WEEK 4/2/HANDSON 3/Country.java
@@ -0,0 +1,25 @@
+package com.cognizant.Rest_HelloWorld;
+
+public class Country {
+	public String code;
+	public String name;
+	public Country(){
+	}
+	public Country(String code, String name) {
+		super();
+		this.code = code;
+		this.name = name;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}	
+}
