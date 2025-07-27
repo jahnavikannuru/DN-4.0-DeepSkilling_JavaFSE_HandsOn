3c80a1f0f6b15462fba0194e2d712ace5fdb8494
jahnavikannuru <jahnavikannuru@gmail.com>
2025-07-27 19:01:44 +0530

    Add files via upload

---
+++ WEEK 6/4.REACTJS-HOL/Posts.js
@@ -0,0 +1,45 @@
+// src/Posts.js
+import React, { Component } from 'react';
+import Post from './Post';
+
+class Posts extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: [],
+    };
+  }
+
+  loadPosts = () => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((res) => res.json())
+      .then((data) => {
+        const posts = data.map(
+          (item) => new Post(item.id, item.title, item.body)
+        );
+        this.setState({ posts });
+      })
+  };
+
+  componentDidMount() {
+    this.loadPosts();
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.posts.map((post) => (
+          <div key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
+  componentDidCatch(error, info) {
+    alert("An error occurred: " + error);
+    console.error("Error caught in Posts:", error, info);
+  }
+}
+export default Posts;
